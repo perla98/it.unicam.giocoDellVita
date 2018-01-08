fc5c0ec5c083de2f698d08c70a2278c1ef8f0687
andrea <andrea.perlini1998@gmail.com>
2018-01-08 09:44:34 +0100

    Aggiunta Classi

---
+++ src/it/unicam/giocoDellVita/Cell.java
@@ -0,0 +1,27 @@
+package it.unicam.giocoDellVita;
+
+public class Cell {
+
+	private boolean state = false;
+    private boolean newState;
+
+    public Cell() {
+
+    }
+
+    public Cell(boolean state) {
+        this.state = state;
+    }
+
+    public void setNewState(boolean state) {
+        newState = state;
+    }
+
+    public void updateState() {
+        state = newState;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+}
